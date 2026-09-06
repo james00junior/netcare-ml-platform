c50d84295b8fcc8a760de64c4fff7a8251ceed77
MashJunior <james.mashiyane.za@gmail.com>
2026-09-06 21:38:56 +0200

    Export serving monitoring contract

--- src/monitoring/__init__.py
+++ src/monitoring/__init__.py
@@ -1,11 +1,31 @@
 from .data_quality import DataQualityResult, validate_required_columns
 from .drift import detect_data_drift
 from .performance import check_performance_degradation, compute_performance_metrics
+from .serving import (
+    ENDPOINT_USAGE_COLUMNS,
+    SERVED_ENTITY_COLUMNS,
+    build_serving_observation,
+    endpoint_usage_query,
+    normalize_endpoint_health,
+    normalize_endpoint_metrics,
+    normalize_usage_rows,
+    resolve_served_entity,
+    served_entities_query,
+)
 
 __all__ = [
     "DataQualityResult",
+    "ENDPOINT_USAGE_COLUMNS",
+    "SERVED_ENTITY_COLUMNS",
+    "build_serving_observation",
     "check_performance_degradation",
     "compute_performance_metrics",
     "detect_data_drift",
+    "endpoint_usage_query",
+    "normalize_endpoint_health",
+    "normalize_endpoint_metrics",
+    "normalize_usage_rows",
+    "resolve_served_entity",
+    "served_entities_query",
     "validate_required_columns",
 ]
