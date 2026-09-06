7feb27aa589039c3dc870eda7328dd78059abcbe
MashJunior <james.mashiyane.za@gmail.com>
2026-09-06 04:50:38 +0200

    style: format serving tests with Black

--- tests/test_serving.py
+++ tests/test_serving.py
@@ -4,7 +4,10 @@ import pandas as pd
 import pytest
 from pydantic import ValidationError
 
-from src.serving.databricks_client import DatabricksServingClient, DatabricksServingError
+from src.serving.databricks_client import (
+    DatabricksServingClient,
+    DatabricksServingError,
+)
 from src.serving.mlflow_model import ReadmissionServingModel
 from src.serving.schemas import PredictionRequest
 
