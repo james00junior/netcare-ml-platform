a2b2fd901cdd0bb6add8febfec106fbaba87c976
MashJunior <james.mashiyane.za@gmail.com>
2026-09-06 19:28:42 +0200

    Export Phase 11 data quality primitive

--- src/monitoring/__init__.py
+++ src/monitoring/__init__.py
@@ -1,8 +1,11 @@
+from .data_quality import DataQualityResult, validate_required_columns
 from .drift import detect_data_drift
 from .performance import check_performance_degradation, compute_performance_metrics
 
 __all__ = [
+    "DataQualityResult",
     "check_performance_degradation",
     "compute_performance_metrics",
     "detect_data_drift",
+    "validate_required_columns",
 ]
